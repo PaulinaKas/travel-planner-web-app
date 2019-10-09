d2e4ec5486628a129a4e0b59b8e157be1ea1498b
Paulina Kasperek <kasperek_p@onet.pl>
2019-10-09 19:03:00 +0200

    Changing path to api_call.txt

--- weather/weather_app/views.py
+++ weather/weather_app/views.py
@@ -2,9 +2,8 @@ from django.shortcuts import render
 import requests
 
 def index(request):
-    url = open('api_call.txt','r').read()
+    url = open('weather_app/api_call.txt','r').read()
     city = 'Cracow'
     city_weather = requests.get(url.format(city)).json() #request the API data and convert the JSON to Python data types
 
     return render(request, 'weather_app/index.html') #returns the index.html template
-    
