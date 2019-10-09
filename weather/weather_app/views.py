fb6746c008c936f2d8745ac071f4f28d916e8f13
Paulina Kasperek <kasperek_p@onet.pl>
2019-10-09 18:56:48 +0200

    Filling in to sending request

--- weather/weather_app/views.py
+++ weather/weather_app/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+import requests
 
 def index(request):
+    url = open('api_call.txt','r').read()
+    city = 'Cracow'
+    city_weather = requests.get(url.format(city)).json() #request the API data and convert the JSON to Python data types
+
     return render(request, 'weather_app/index.html') #returns the index.html template
