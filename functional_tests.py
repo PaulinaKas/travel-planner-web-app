47993cc82e4f2caa1c333873f97e772bdab633df
Paulina Kasperek <kasperek_p@onet.pl>
2019-10-10 21:06:36 +0200

    Checking if websites title = "Current weather" - passed

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 
 browser = webdriver.Chrome()
-browser.get('https://localhost:8000')
+browser.get('http://127.0.0.1:8000/')
 
-assert 'something' in browser.title
+assert 'Current weather' in browser.title
