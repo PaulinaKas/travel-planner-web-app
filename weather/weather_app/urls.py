ee74db7f60d66fa141559471ccca7559f97a7fa5
Paulina Kasperek <49270882+PaulinaKas@users.noreply.github.com>
2020-03-09 22:00:39 +0100

    Url pattern for deleting function added

--- weather/weather_app/urls.py
+++ weather/weather_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    path('', views.display_forecast),  
+    path('', views.display_forecast),
+    url(r'^delete/(?P<pk>[0-9]+)/$', views.delete_city, name='delete_city'),
 ]
