ee9e64db4b20d7d7fc95366655d2390c8e12e3ff
Paulina Kasperek <49270882+PaulinaKas@users.noreply.github.com>
2020-03-09 21:21:34 +0100

    Refactoring: removing needless comments and correction of variables names

--- weather/weather_app/views.py
+++ weather/weather_app/views.py
@@ -5,22 +5,19 @@ from .forms import CityForm
 import requests
 
 
-def index(request):
+def display_forecast(request):
     url = open('weather_app/api_keys.txt','r').read()
-    cities = City.objects.all() #return all the cities in the database
+    cities = City.objects.all()
 
-    if request.method == 'POST': # only true if form is submitted
-        form = CityForm(request.POST) # add actual request data to form for processing
-        form.save() # will validate and save if validate
+    if request.method == 'POST':
+        form = CityForm(request.POST)
+        form.save()
 
     form = CityForm()
-
     weather_data = []
 
     for city in cities:
-
-        city_weather = requests.get(url.format(city)).json() #request the API data and convert the JSON to Python data types
-
+        city_weather = requests.get(url.format(city)).json()
         weather = {
             'city' : city,
             'temperature' : city_weather['main']['temp'],
@@ -28,9 +25,8 @@ def index(request):
             'description' : city_weather['weather'][0]['description'],
             'icon' : city_weather['weather'][0]['icon']
         }
-
-        weather_data.append(weather) #add the data for the current city into our list
+        weather_data.append(weather)
 
     context = {'weather_data' : weather_data, 'form' : form}
 
-    return render(request, 'index.html', context) 
+    return render(request, 'index.html', context)
