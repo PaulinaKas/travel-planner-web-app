8d9c725ade1f27bed12e4b81cb87afe5e2c69c86
Paulina Kasperek <49270882+PaulinaKas@users.noreply.github.com>
2020-03-10 18:35:33 +0100

    Commenting one line

--- weather/weather_app/urls.py
+++ weather/weather_app/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.display_forecast),
     url(r'^delete/(?P<pk>[0-9]+)/$', views.delete_city, name='delete_city'),
+    # url(r'^schedule/(?P<pk>[0-9]+)/$', views.add_schedule, name='add_schedule'),
 ]
