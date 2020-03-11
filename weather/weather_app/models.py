14cb257e7afcf2afd754ca110f6891d04cdf4f86
Paulina Kasperek <49270882+PaulinaKas@users.noreply.github.com>
2020-03-11 02:16:23 +0100

    models.ForeignKey added

--- weather/weather_app/models.py
+++ weather/weather_app/models.py
@@ -3,14 +3,9 @@ from django.db import models
 class City(models.Model):
     name = models.CharField(max_length=25)
 
-    def __str__(self):
-        return self.name
-
     class Meta: # show the plural of city as cities instead of citys
         verbose_name_plural = 'cities'
 
 class TravelPlan(models.Model):
-     text = models.TextField()
-
-     def __str__(self):
-        return self.text
+    text = models.TextField()
+    name = models.ForeignKey(City, null=True, on_delete=models.CASCADE)
